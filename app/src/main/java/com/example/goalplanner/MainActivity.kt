0bf29f07e6b63c38edbf72310ce9867dd77611e5
gjgustjd <gjgustjd70@naver.com>
2022-01-01 23:47:57 +0900

    액션바 및 상단바 해제

--- app/src/main/java/com/example/goalplanner/MainActivity.kt
+++ app/src/main/java/com/example/goalplanner/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.goalplanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            }
     }
-}

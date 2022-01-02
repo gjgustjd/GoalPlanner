63f85af46c975840f0ccebd80dc433e51990dc39
gjgustjd <gjgustjd70@naver.com>
2022-01-02 22:56:12 +0900

    tabLayout viewBinding으로 연결

--- app/src/main/java/com/example/goalplanner/MainActivity.kt
+++ app/src/main/java/com/example/goalplanner/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.goalplanner
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.goalplanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
             }
+
+
     }
